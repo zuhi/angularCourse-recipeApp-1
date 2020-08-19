031e1ce334fc1c91007be065e198798c3c295e79
Ankita Shreya <ankita9shreya@gmail.com>
2020-08-20 00:19:37 +0530

    Using Services to Push Data from A to B ,and emitting event in service to reflect changes in B

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, EventEmitter, Output } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient-model';
+import { ShoppingListService } from '../shopping-list.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -10,16 +11,19 @@ export class ShoppingEditComponent implements OnInit {
   @ViewChild('nameInput',{static:true}) nameInputRef: ElementRef;
   @ViewChild('amountInput',{static:true}) amountInputRef: ElementRef
   //ingredientAdded = new EventEmitter<{name: string, amount: number}>();  //substitue in below lines..
-  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+ // @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
-  constructor() { }
+  constructor(private slService: ShoppingListService) { }
 
   ngOnInit(): void {
   }
   onAddItem() {
     console.log("COming to child add!");
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
     const newIngredient = new Ingredient(this.nameInputRef.nativeElement.value,this.amountInputRef.nativeElement.value);
-    this.ingredientAdded.emit(newIngredient);
+    //this.ingredientAdded.emit(newIngredient);
+    this.slService.addIngredient(newIngredient);
    
   }
 }
