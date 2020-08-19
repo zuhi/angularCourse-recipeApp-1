c7d62f4387d1f658840ab3b66122f7e9b9117849
Ankita Shreya <ankita9shreya@gmail.com>
2020-08-20 00:37:02 +0530

    Adding ingredients to recipe details

--- src/app/recepies/recipe.service.ts
+++ src/app/recepies/recipe.service.ts
@@ -1,12 +1,32 @@
 import { Recipe } from '../recipe';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient-model';
 
 export class RecipeService {
    recipeSelected = new EventEmitter<Recipe>();
    private recipes: Recipe[] = [
-        new Recipe("A Test Recipe", "This is simple a test", "https://www.stonewallkitchen.com/dw/image/v2/AAYB_PRD/on/demandware.static/-/Sites-swk-catalog/default/dw5152ecc3/R444_grid.jpg?sw=350&sh=350"), 
-        new Recipe("Second Recipe", "This is my second recipe" ,"https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg"),
-        new Recipe("Third Recipe", "This is my third recipe" ,"https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg")
+        new Recipe(
+         "A Test Recipe", 
+         "This is simple a test", 
+         "https://www.stonewallkitchen.com/dw/image/v2/AAYB_PRD/on/demandware.static/-/Sites-swk-catalog/default/dw5152ecc3/R444_grid.jpg?sw=350&sh=350",
+         [
+            new Ingredient('Meat', 16),
+            new Ingredient('French Fries', 2)
+
+         ]), 
+        new Recipe(
+           "Second Recipe", 
+           "This is my second recipe" ,
+           "https://www.wellplated.com/wp-content/uploads/2017/12/Hoppin-John-recipe-600x629.jpg",
+           []),
+        new Recipe(
+           "Third Recipe", 
+           "This is my third recipe" ,
+           "https://www.tasteofhome.com/wp-content/uploads/2017/10/Healthier-than-Egg-Rolls_EXPS_SDON17_55166_C06_23_6b-696x696.jpg",
+           [
+            new Ingredient('Buns', 10),
+            new Ingredient('White sauce', 2)
+           ])
       ];
        
    getRecipes() {
--- src/app/recipe.ts
+++ src/app/recipe.ts
@@ -1,10 +1,14 @@
+import { Ingredient } from './shared/ingredient-model';
+
 export class Recipe {
     public name: string;
     public description: string;
     public imagePath: string;
-    constructor( name: string, desc: string, imgPath: string){
+    public ingredients: Ingredient[];
+    constructor( name: string, desc: string, imgPath: string, ingredients: Ingredient[]){
         this.name = name;
         this.description = desc;
         this.imagePath = imgPath;
+        this.ingredients = ingredients;
     }
 }
